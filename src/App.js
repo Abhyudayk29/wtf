64b1b885958fbc548b719aac1d41bf31b67f4c88
Abhyudayk29 <kmr.abd7@gmail.com>
2023-11-20 12:53:03 +0530

    reverted

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import './App.css';
 const App = () => (
   <div className="app">
     <Navbar />
-    <div className="section"><About /></div>
+    <div className="section"><About />
+    <div id="data-container">
+        <h2>Data for Selected Country</h2>
+        <div id="country-data"></div>
+      </div>
+    </div>
     <div className="section"><Work /></div>
     <div className="section"><Skills /></div>
   </div>
 );
+
+
 
 export default App;
